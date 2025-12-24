89850cf5b270660a12f61935a59aa520618fedf7
ViLan026 <vil782991@gmail.com>
2025-12-24 16:52:09 +0700

    thêm avatar cho learner

--- src/main/java/com/example/tutorsFinderSystem/dto/response/LearnerProfileResponse.java
+++ src/main/java/com/example/tutorsFinderSystem/dto/response/LearnerProfileResponse.java
@@ -1,16 +1,18 @@
 package com.example.tutorsFinderSystem.dto.response;
 
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class LearnerProfileResponse {
 
     private String fullName;
     private String phoneNumber;
     private String address;
-
+    private String avatarUrl;
     private String email;
-    // private String roleLabel;   // Người học
+    // private String roleLabel; // Người học
 }
